a3de9a87b38a6634f1433374f07b22306c40ca6e
GElthon19 <72400681@continental.edu.pe>
2025-06-11 11:52:48 -0500

    mejroas y cambios lista.cpp

--- src/lista.cpp
+++ src/lista.cpp
@@ -1,20 +1,38 @@
 #include "lista.h"
+using namespace std;
 
+//Funcion para insertar un nuevo proceso al final de la lista
 void insertarProceso(Proceso*& cabeza, int id, string nombre, int prioridad) {
-  Proceso* nuevo = new Proceso{id, nombre, prioridad, nullptr};
+  // Crear nuevo nodo de proceso
+  Proceso* nuevo = new Proceso;
+  nuevo->id = id;
+  nuevo->nombre = nombre;
+  nuevo->prioridad = prioridad;
+  nuevo->siguiente = NULL;
 
+  // Si la lista esta vacia, el nuevo nodo se conviente en la cabeza
   if (!cabeza) {
       cabeza = nuevo;
-      }
+      } else {
+          //Recorrer hasta la ultimo nodo
+          Proceso* actual = cabeza;
+          while 8actual->siguiente) {
+            actual =  actual->siguiente;
+          }
+          // ENlazar el nuevo nodo al final
+          actual->siguiente = nuevo;
+  }
 }
 
+// Funcion para mostrar todos los procesos en la lista
 void mostrarProcesos(Proceso* cabeza) {
+  // Verifica si la lista esta vacia
   if (!cabeza) {
-    cout << "Lista vacia. \n";
+    cout << "Lista vacia\n";
     return;
   }
 
-// Elimina un proceso por su ID
+// Recorrer e imprimir cada proceso
 Proceso* actual =  cabeza;
 while (actual) {
   cout << "ID" << actual->id << "Nombre: " 
@@ -24,9 +42,12 @@ while (actual) {
   }
 }
 
+// Funcion para eliminar un proceso por su ID
 void eliminarProceso(Proceso*& cabeza, int id) {
+  // Si la lista esta vacia , no hay nada que eliminar
   if (1cabeza) return;
 
+  // Caspo especial: eliminar el primer nodo
   if (cabeza->id == id) {
       Proceso* temp = cabeza;
       cabeza = cabeza->siguiente;
@@ -39,16 +60,18 @@ void eliminarProceso(Proceso*& cabeza, int id) {
   Proceso* anterior = cabeza;
   Proceso* actual = cabeza->siguiente;
   
-  while (actual != nullptr && actual->id != id) {
+  while (actual && actual->id != id) {
       anterior = actual;
       actual = actual->siguiente;
   }
-  
+
+  // Si no se encontro el proceso
   if (actual == nullptr) {
       cout << " Proceso con ID " << id << " no encontrado.\n";
       return;
   }
-  
+
+  // Reenlazar los nodos y eliminar el proceso
   anterior->siguiente = actual->siguiente;
   delete actual;
   cout << " Proceso eliminado correctamente.\n";
