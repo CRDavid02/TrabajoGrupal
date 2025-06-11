51f4b143965f95fb41ec37ab77db388049ff4e02
johan20254 <76246168@continental.edu.pe>
2025-06-10 19:58:13 -0500

    Update pila.cpp

--- src/pila.cpp
+++ src/pila.cpp
@@ -1,1 +1,25 @@
-
+#inclaude "pila.h"
+void pushiHistorial(NodoPia*& cima, const string& desc) {
+  NodoPila* nuevo = new NodoPila{desc, cima;
+  cima = nuevo;
+}
+void popHistorial(NodoPila*& cima) {
+  if (!cima) {
+cout << " Historial vacio. \n";
+return;
+  }
+NodoPila* temp = cima;
+cima = cima -> siguiente;
+delete temp;
+}
+void verHistorial(NodoPila* cima) {
+  if (!cima) {
+cout <<"Hiatorial vacio. \n";
+return;
+  }
+NodoPila* actual = cima;
+while (actual) {
+cout << actual -> descripcion << endl;
+actual = actual ->siguiente;
+  }
+}
