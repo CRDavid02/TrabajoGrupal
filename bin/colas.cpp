9072e9bff18a80e24741a814eb0020a8f2862848
CRDavid02 <73392625@continental.edu.pe>
2025-06-10 19:56:26 -0500

    codigo cpp

--- bin/colas.cpp
+++ bin/colas.cpp
@@ -1,1 +1,33 @@
+#include "cola.h"
+
+void encolar(NodoCola*& frente, NodoCola*& fin, Proceso* proceso) {
+    Nodo Cola* nuevo = new NodoCola{proceso, nullptr);
+
+    if(!frente) {
+        frente = fin = nuevo;
+    } else {
+        fin->siguiente = nuevo;
+        fin = nuevo;
+    }
+}
+
+Proceso* desencolar(NodoCola*& frente, NodoCola*& fin) {
+    if (!frente) return nullptr;
+
+    NodoCola* temp = frente;
+    Proceso* proceso = temp->proceso;
+    frente = frente->siguiente;
+
+    if (!frente) fin = nullptr;
+    delete temp;
+    return proceso;
+}
+
+void mostrarCola(NodoCola* frente) {
+    NodoCola* actual = frente;
+    while (actual) {
+        cout << "Proceso: " << actual->proceso->nombre<<"| ID: " << actual->proceso->id<< endl;
+        actual = actual-> siguiente;
+    }
+}
 
